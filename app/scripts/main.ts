a26f8c749c68e68ee438c3f94f647dba0c74d1f1
ozaki <fs65ugumori@yahoo.co.jp>
2016-01-06 08:11:57 +0900

    main.tsを作成

---
+++ app/scripts/main.ts
@@ -0,0 +1,33 @@
+/// <reference path="../../typings/tsd.d.ts" />
+
+'use strict';
+
+require.config({
+  shim: {
+    bootstrap: {
+      deps: ['jquery'],
+      exports: 'jquery'
+    },
+    jquery: {
+      exports: '$'
+    },
+    underscore: {
+      exports: '_'
+    }
+  },
+  paths: {
+    jquery: '../bower_components/jquery/dist/jquery',
+    backbone: '../bower_components/backbone/backbone',
+    underscore: '../bower_components/lodash/dist/lodash',
+    bootstrap: '../bower_components/bootstrap-sass-official/assets/javascripts/bootstrap'
+  }
+});
+
+require([
+  'jquery',
+  'backbone',
+  './routes/todo'
+], function ($, Backbone, Router) {
+  var router = new Router.Router();
+  Backbone.history.start();
+});
